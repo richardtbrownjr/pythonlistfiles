80af8f5554ae33fd4110998d0c757619b03d161d
Richard T. Brown Jr <richardtbrownjr@gmail.com>
2017-01-01 19:35:54 -0500

    send up short python script

---
+++ script2.py
@@ -0,0 +1,8 @@
+import os
+for root, dirs, files in os.walk(".", topdown=True):
+    for name in files:
+        print(os.path.join(root, name))
+    for name in dirs:
+        print(os.path.join(root, name))
+
+# This small script runs from the python shell and prints all the files in that folder
